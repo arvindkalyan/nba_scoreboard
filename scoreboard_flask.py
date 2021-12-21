0d8a6087ef8465c92fa23f8bfe5a647db99c6143
Arvind Kalyan <arvindkrishnakalyan@gmail.com>
2021-12-21 00:49:30 -0800

    Ajax pulls to scoreboard complete, testing update on scoreboard change pending Play-by-play with basic JSON pull complete

--- scoreboard_flask.py
+++ scoreboard_flask.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import urllib.request, json 
+import datetime
 
 app = Flask(__name__)
 
-def get_scores(data):
-    scoreline = ""
-    for item in data["games"]:
-        scoreline += item["vTeam"]["triCode"] + " " + item["vTeam"]["score"] + " " + item["hTeam"]["triCode"] + " " + item["hTeam"]["score"] + "\n"
-    return scoreline
+def refresh_data(link):
+    with urllib.request.urlopen(link) as url:
+        return json.loads(url.read().decode())
 
+@app.route('/<string:datestring>/<string:gamestring>/<string:tricodes>') 
+def game(datestring, gamestring, tricodes): 
+    pbp = "https://data.nba.net/data/10s/json/cms/noseason/game/{}/{}/pbp_all.json".format(datestring, gamestring)
+    with urllib.request.urlopen(pbp) as url:
+        plays = json.loads(url.read().decode())
 
+    return render_template('game.html', plays = plays["sports_content"]["game"]["play"], datestring = datestring, visitor = tricodes[:3], home = tricodes[3:])
+
+@app.route('/<string:datestring>') 
+def scoreboard(datestring): 
+    scoreboard = "https://data.nba.net/prod/v1/{}/scoreboard.json".format(datestring)
+    with urllib.request.urlopen(scoreboard) as url:
+        scores = json.loads(url.read().decode())
+    return render_template('get_json.html', scores = scores["games"], datestring = datestring)
 
 @app.route('/')
 def index():
-    scoreboard = "https://data.nba.net/prod/v1/20211212/scoreboard.json"
-    with urllib.request.urlopen(scoreboard) as url:
-        scores = json.loads(url.read().decode())
-    return render_template('index.html', scores = scores["games"])
+    dt = datetime.datetime.today()
+    datestring = "{}{}{}".format(f"{dt.year:02}", f"{dt.month:02}", f"{dt.day:02}")
+    return scoreboard(datestring)
+    #return redirect("/"+datestring, code=302)
+
+@app.route('/test') 
+def test(): 
+    return render_template('get_json.html')
